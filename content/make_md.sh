1ba9d5310fed25bdb17df5c054219a92f39d81eb
Matthew L. Bowser <matt_bowser@fws.gov>
2020-07-07 08:03:00 -0800

    Fix missing images in compiled markdown version

--- content/make_md.sh
+++ content/make_md.sh
@@ -1,3 +1,6 @@
-./pandoc *.md --filter pandoc-citeproc --toc --toc-depth=2 -o compiled/Journal_2020.md -t gfm -s
 
-./pandoc compiled/Journal_2020.md --css style.css --metadata title="Journal 2020" -o compiled/Journal_2020.epub
+rm  Journal_2020.md
+
+./pandoc *.md --filter pandoc-citeproc --toc --toc-depth=2 -o Journal_2020.md -t gfm -s
+
+./pandoc Journal_2020.md --css style.css --metadata title="Journal 2020" -o compiled/Journal_2020.epub
