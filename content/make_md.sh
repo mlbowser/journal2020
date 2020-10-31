87d1f2835dff620c56dd662a545efc71b9fe316f
Matthew L. Bowser <matt_bowser@fws.gov>
2020-10-31 08:12:04 -0800

    Add content through October 31

--- content/make_md.sh
+++ content/make_md.sh
@@ -4,3 +4,7 @@ rm  Journal_2020.md
 pandoc *.md --filter pandoc-citeproc --toc --toc-depth=2 -o Journal_2020.md -t gfm -s
 
 pandoc Journal_2020.md --css style.css --metadata title="Journal 2020" -o compiled/Journal_2020.epub
+
+pandoc 01_title.yaml Journal_2020.md -o compiled/Journal_2020.docx
+
+# pandoc Journal_2020.md -o compiled/Journal_2020.pdf
