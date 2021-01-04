582aebf699f9846da50a207be9f92aad2075489f
Matthew L. Bowser <matt_bowser@fws.gov>
2021-01-04 06:55:27 -0900

    Add cover image

--- content/make_md_h.sh
+++ content/make_md_h.sh
@@ -55,7 +55,9 @@ rm Journal_2020-*.md
  
 #pandoc *.md --filter pandoc-citeproc --toc --toc-depth=2 -o Journal_2020.md -t gfm -s
 
-pandoc Journal_2020.md --css style.css --metadata title="Journal 2020" -o compiled/Journal_2020.epub
+#pandoc Journal_2020.md --css style.css --metadata title="Work Journal 2020" -o compiled/Journal_2020.epub
+
+pandoc Journal_2020.md --css style.css --metadata-file=01_title.yaml --epub-cover-image=cover.jpg -o compiled/Journal_2020.epub
 
 #pandoc 01_title.yaml Journal_2020.md -o compiled/Journal_2020.docx
 
